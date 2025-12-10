ac3d0b8eb163e594aae41921d66f987677a13c4d
demo <codeiodemo@gmail.com>
2025-12-10 13:49:52 +0530

    new change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Welcome to herbal organic world!";
+    private static final String MESSAGE = "Welcome to java coding!";
 
     public App() {}
 
